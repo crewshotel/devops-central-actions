9ab9c0402e1200f1d774e0826992263b96f17979
Rodney Newberry <rodney.newberry@groups360.com>
2022-11-17 14:42:15 -0600

    update1117-1442

---
+++ scripts/aws/docker/list-k8s-nodes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create variables
+DOCKERSUB=$1
+REGION=$2
+CLUSTER=$3
+
+DOCKER_REGISTRY=${DOCKERSUB}.dkr.ecr.${REGION}.amazonaws.com
+
+DOCKER_IMAGE_FULL=${DOCKER_REGISTRY}/admin-${CLUSTER}:latest
+
+# start docker image
+docker run -d -ti --entrypoint /bin/bash ${DOCKER_IMAGE_FULL}
+
+# list docker images
+docker image ls -a
